a15bc48934500b947b86aa241d1a24e04bdfcb1c
unknown <AOAOOAOAOAOA@Github.com>
2024-09-16 15:13:10 +0300

    Added code from lesson for server

--- CofServer/Program.cs
+++ CofServer/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Код с урока
+using System.Net;
+using System.Text;
+
+string serverUrl = "http://localhost:5000";
+
+var server = new HttpListener();
+
+server.Prefixes.Add(serverUrl + "/create/");
+server.Prefixes.Add(serverUrl + "/get-all/");
+server.Prefixes.Add(serverUrl + "/get-later/");
+server.Prefixes.Add(serverUrl + "/delete/");
+server.Start();
+
+while (true)
+{
+    var context = server.GetContext();
+    var request = context.Request;
+    var response = context.Response; 
+
+    if (request.Url.AbsoluteUri == (serverUrl + "/create/"))
+    {
+        byte[] buffer = new byte[request.ContentLength64];
+        request.InputStream.Read(buffer, 0, buffer.Length);
+
+        string requestBody = Encoding.UTF8.GetString(buffer);
+
+        Console.WriteLine(requestBody);
+    }
+
+
+}
+
+
+
