96db11b6fcfe22676cdd3f4dfe759615212fa81d
unknown <AOAOOAOAOAOA@Github.com>
2024-09-21 02:43:49 +0300

    2-st quest

--- CofServer/Program.cs
+++ CofServer/Program.cs
@@ -1,9 +1,8 @@
-﻿//Код с урока
-using System.Net;
+﻿using System.Net;
 using System.Text;
 using System.Text.Json;
 
-string serverUrl = "http://localhost:5000";
+const string serverUrl = "http://localhost:5000";
 
 var server = new HttpListener();
 
@@ -23,27 +22,48 @@ while (true)
 
     if (request.Url.AbsoluteUri == (serverUrl + "/create/"))
     {
-        byte[] buffer = new byte[request.ContentLength64];
-        request.InputStream.Read(buffer, 0, buffer.Length);
-
-        string jsonstring = Encoding.UTF8.GetString(buffer);
-
-        try
-        {
-            CoffeeOrder _order = JsonSerializer.Deserialize<CoffeeOrder>(jsonstring);
-            _order.PrintOrderInfo();
-            CoffeOrderList.Add(_order);
-            
-        }
-        catch (Exception ex)
-        {  
-            Console.WriteLine($"ERROR \n TEXT TYPE:{jsonstring} IS NOT JSON DESERIALIZEBLE FORMAT");
-        }
         
+        
+    }
+
+    switch(request.Url.AbsoluteUri)
+    {
+        case serverUrl + "/create/":
+            byte[] buffer = new byte[request.ContentLength64];
+            request.InputStream.Read(buffer, 0, buffer.Length);
+
+            string jsonstring = Encoding.UTF8.GetString(buffer);
+
+            try
+            {
+                CoffeeOrder _order = JsonSerializer.Deserialize<CoffeeOrder>(jsonstring);
+                _order.PrintOrderInfo();
+                CoffeOrderList.Add(_order);
+
+            }
+            catch (Exception ex)
+            {  
+                Console.WriteLine($"ERROR \n TEXT TYPE:{jsonstring} IS NOT JSON DESERIALIZEBLE FORMAT");
+            }
+            break;
+            
+        case serverUrl + "/get-all/":
+            foreach (var order in CoffeOrderList)
+            {
+                string jsonResponse = JsonSerializer.Serialize(order, new JsonSerializerOptions { WriteIndented = true });
+                byte[] res = Encoding.UTF8.GetBytes(jsonResponse);
+                await response.OutputStream.WriteAsync(res, 0, res.Length);
+            }
+            response.OutputStream.Close();
+            break;
+
+
     }
 
 
 }
+
+
 
 
 
